2f61cd83d6aadfe50e6bfb709a6cedd320693933
Michael Groeneman <groenemm@carleton.edu>
2011-06-28 15:58:40 -0500

    Revert "Deletion of threaded_parser.py (changed name to parser.py)"
    This reverts commit 62f5593975eb759197c40871456a89b798f35226.

--- newParser.py
+++
@@ -1,9 +0,0 @@
-import bz2
-from wmf import dump
-path = "../enwiki-20110405-pages-meta-current1.xml.bz2"
-
-fp = bz2.BZ2File(path,"r")
-dumpIterator = dump.Iterator(fp)
-
-for page in dumpIterator.readPages():
-	print(page.getText())
