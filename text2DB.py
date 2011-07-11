a6145380fb5345a098655125eab8d0b75ab8f8a0
Michael Groeneman <groenemm@carleton.edu>
2011-07-11 16:04:00 -0500

    Added text2DB.py, which pulls text and inserts it into a database using Psycopg

---
+++ text2DB.py
@@ -0,0 +1,96 @@
+import sys
+import xml.sax
+from collections import defaultdict,namedtuple
+from bz2 import BZ2File
+import multiprocessing
+import psycopg2
+
+class PageHandler(xml.sax.handler.ContentHandler):
+	def __init__(self,outQ):
+		self.buffer =[]
+		self.inText = False
+		self.inID = False
+		self.inRevision = False
+		self.currentPageID = None
+		self.outQ = outQ
+		
+	def startElement(self, name, attributes):
+		if name == "text":
+			self.inText = True
+		elif name == "id":
+			self.inID= True
+		elif name == "revison":
+			self.inRevision = False
+		
+	def characters(self, data):
+		if (self.inID and not self.inRevision) or self.inText:
+			self.buffer.append(data)
+		
+	def endElement(self, name):
+		if name == "id":
+			self.inID= False
+			if not self.inRevision:
+				self.currentPageID = "".join(self.buffer)
+		elif name == "revision":
+			self.inRevision = False
+		elif name == "text":
+			self.inText = False
+			text = "".join(self.buffer)
+			self.outQ.put((self.currentPageID,text))
+		elif name == "mediawiki":
+			self.outQ.put((None,None))
+		
+		self.buffer = []
+
+def qManager(outQ,connection):
+	i=1
+	cursor = connection.cursor()
+	while True:
+		pageID,text = outQ.get()
+		if pageID is not None:
+			addToDB(pageID,text,cursor)
+			print i
+			i+=1
+		else:
+			break
+
+def addToDB(pageID, pageText, cursor):
+    cursor.execute("INSERT INTO articleText VALUES (%s, %s);", (pageID, pageText))
+
+def readIn(inQ):
+	chunksize=10000000
+	infile = BZ2File(sys.argv[1])
+	next = infile.read(chunksize)
+	while next != "":
+		inQ.put(next)
+		next = infile.read(chunksize)
+	inQ.put(None)
+
+#page = namedtuple("Page",['pageid','text'])
+
+if __name__=="__main__":
+    connection = psycopg2.connect(database = "wikigroup", user = "postgres", password = "wiki!@carl", host = "localhost", port = "5432")
+	
+	parser = xml.sax.make_parser()
+	inQ = multiprocessing.Queue(10)
+	outQ = multiprocessing.Queue(50)
+	handler = PageHandler(outQ)
+	parser.setContentHandler(handler)
+
+	p = multiprocessing.Process(target=qManager,args=(outQ,connection,))
+	p.start()
+	q = multiprocessing.Process(target=readIn,args=(inQ,))
+	q.start()
+	
+	
+	
+	chunk = inQ.get()
+	while True:
+		if chunk is not None:
+			parser.feed(chunk)
+			chunk = inQ.get()
+		else:
+			break
+		
+	p.join()
+	q.join()
